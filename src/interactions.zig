78aa7b606aa5d538d9e897b5b379d66506c30e4c
Diego Ugarte <diego.ugarte@outlook.com>
2021-08-26 04:16:26 +0900

    :fire: Delete unsued filess

--- src/interactions.zig
+++
@@ -1,5 +0,0 @@
-pub const LennardJonesInteraction = @import("interactions/lennard_jones.zig").LennardJonesInteraction;
-
-pub const Interactions = struct {
-    lennard_jones: ?[]LennardJonesInteraction = null,
-};
--- src/interactions/lennard_jones.zig
+++
@@ -1,7 +0,0 @@
-const Real = @import("../config.zig").Real;
-
-pub const LennardJonesInteraction = struct {
-    id: u64 = undefined,
-    e: Real = undefined,
-    s: Real = undefined,
-};
