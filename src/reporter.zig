7244e999b1d34033689ccae7b1618be2ef7261b4
Diego Ugarte <diego.ugarte@outlook.com>
2021-09-13 00:14:51 +0900

    :construction: Prototype reporter

---
+++ src/reporter.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+// var reporter = Reporter.init(allocator, &system);
+// defer reporter.deinit();
+//
+// try reporter.addPosReporter(file_name, .XYZ, 20);
+// try reporter.addVelReporter(file_name, .VEL, 20);
+// try reporter.addLogReporter(file_name, .LOG, 20);
+//
+// try reporter.reportPos(current_time);
+// try reporter.reportVel(current_time);
+// try reporter.reportLog(current_time);
+//
+// try reporter.report(current_time);
+//
+
+pub const Reporter = struct {};
